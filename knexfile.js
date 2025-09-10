ddd9e7502bccf24fc726fc5ce812e64b3e41b268
Júlia Arapiraca <94020003+juliaarapiraca@users.noreply.github.com>
2025-09-10 16:23:34 -0300

    att

--- knexfile.js
+++ knexfile.js
@@ -6,22 +6,13 @@ module.exports = {
     connection: {
       connectionString: process.env.DB_HOST,
       ssl: { rejectUnauthorized: false }
-    },
-    migrations: {
-      directory: "./migrations"
-    },
-    seeds: {
-      directory: "./seeds"
     }
   },
   development: {
     client: "pg",
-    connection: process.env.DB_HOST,
-    migrations: {
-      directory: "./migrations"
-    },
-    seeds: {
-      directory: "./seeds"
+    connection: {
+      connectionString: process.env.DB_HOST,
+      ssl: { rejectUnauthorized: false }
     }
   }
 };
